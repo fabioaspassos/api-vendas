8410d620d00bde7a7ef85810e29221a4f1b13acf
Fabio Passos <phabiopassos@gmail.com>
2021-12-28 11:04:52 -0300

    products route

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
+
+const routes = Router();
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello' });
+});
+
+routes.use('/products', productsRouter);
+
+export default routes;
